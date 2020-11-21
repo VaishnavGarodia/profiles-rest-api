60d7f79911707ad4c8aec222bf8761e93dbd1b31
Vaishnav <garodiavaishnav@gmail.com>
2020-11-21 09:19:38 +0000

    added custom user profile model manager and migrations

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,7 +1,40 @@
 from django.db import models
-from django.contib.auth.models import AbstractBaseUser
-from django.contib.auth.models import PermissionsMixin
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
+
+
+class UserProfileManager(BaseUserManager):
+    """Manager for User Profiles"""
+
+    def create_user(self, email, name, password=None):
+        """Create a new User Profile"""
+        if not email:
+            raise ValueError("Users must have a email address")
+
+        email = self.normalize_email(email)
+        user = self.model(email = email, name= name)
+
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, name, password):
+        """ create and save a new superuser with given details"""
+        user = self.create_user(email, name, password)
+
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+
+        return user
+
+
+
 class UserProfile(AbstractBaseUser, PermissionsMixin):
+    """Database Model for users in the system"""
+
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
@@ -18,4 +51,3 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
         return self.name
     def __str__(self):
         return self.email
-        
