983cae7f5175c2a965d4c2704976894be28530bf
Vaishnav <garodiavaishnav@gmail.com>
2020-12-07 17:01:12 +0000

    Added APIView

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,40 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from profiles_api import serializers
+
 
 # Create your views here.
+
+class APIView(APIView):
+    """Test API view"""
+    serializer_class = serializers.HelloSerializer
+    def get(self, request, format=None):
+        """Returns a list of APIView features"""
+
+        api_view = ["1","2","3","4"]
+
+        return Response({"message":"hello", "api_view":api_view})
+
+    def post(self, request):
+        """Post your name"""
+
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            name = serializer.validated_data.get("name")
+            message = f'hello {name}'
+            return Response({"message":message})
+        else:
+            return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk=None):
+        """Handles updating an object"""
+        return Response({"method":"put"})
+    def patch(self, request):
+        """Handles a partial update of an object"""
+        return Response({"method":"patch"})
+    def delete(self, request):
+        """Delete an Object"""
+        return Response({"method":"delete"})
+                
--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '8tuegy2o1@qw6dk%(3$^7%1o(@abl3^_anb1++^_0d=of^8cv&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
